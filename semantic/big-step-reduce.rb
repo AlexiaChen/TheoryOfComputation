9b2607666df993b33cb6eab7d7f66fe544a2fb5a
MathxH Chen <brainfvck@foxmail.com>
2017-10-18 21:19:26 +0800

    Number support

--- semantic/big-step-reduce.rb
+++ semantic/big-step-reduce.rb
@@ -6,3 +6,19 @@
     simply run command irb with '--simple-prompt' agrument
 =end
 
+class Number < Struct.new(:value)
+  def to_s
+    value.to_s
+  end
+
+  def evaluate(var_eniroment)
+    self
+  end
+      
+  def inspect
+    "<<#{self}>>"
+  end
+end
+
+# => 23
+Number.new(23).evaluate({})
