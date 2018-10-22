9ccd44d801fd8bfce491f2aee2d5df869a41b678
MathxH Chen <brainfvck@foxmail.com>
2018-10-22 22:27:03 +0800

    [+]: Add nfa rule and ruleset class

--- machine/nfa.rb
+++ machine/nfa.rb
@@ -4,3 +4,33 @@
   how to use:
     simply run command irb with '--simple-prompt' agrument
 =end
+
+require 'set'
+
+class FARule < Struct.new(:state, :character, :next_state)
+  def applies_to?(state,character)
+    self.state == state && self.character == character
+  end
+
+  def follow
+    next_state;
+  end
+
+  def inspect
+    "#<FARule #{state.inspect} --#{character}--> #{next_state}>"
+  end
+end
+
+class NFARuleSet < Struct.new(:rules)
+  def next_states(states, character)
+    states.flat_map { |state| follow_rules_for(state, character) }.to_set
+  end
+
+  def follow_rules_for(state, character)
+    rules_for(state, character).map(&:follow)
+  end
+
+  def rules_for(state, character)
+    rules.select { |rule| rule.applies_to?(state, character) } # select * where 
+  end
+end
